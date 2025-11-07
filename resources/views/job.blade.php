b5eb0c32fb17bf6d0953dfab373db8ad16fe5c66
Ahmad khan <hadijack365@gmail.com>
2025-11-07 09:48:27 +0500

    wildcards

---
+++ resources/views/job.blade.php
@@ -0,0 +1,14 @@
+<x-layout>
+    <x-slot:heading>
+        Jobs
+    </x-slot:heading>
+    @foreach ($jobs as $job)
+
+        <ul>
+            <li><a href="/jobs/">
+                    <b> {{ $job['title'] }} </b>: Pays {{ $job['salary'] }} per year.
+                </a></li>
+        </ul>
+
+    @endforeach
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,39 @@
 <?php
-
+use  \Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about' , function(){
-    return view('about');
+Route::get('/jobs/{id}', function ($id) {
+    
+        $jobs = [
+            [
+                'id' => 1,
+                'title' => 'Director',
+                'salary' => '$50,000'
+            ],
+
+            [
+                'id' => 2,
+                'title' => 'programmer',
+                'salary' => '$10,000'
+            ],
+
+            [
+                'id' => 3,
+                'title' => 'Teacher',
+                'salary' => '$30,000'
+            ]
+
+            ];
+        
+        
+        $job = Arr::first($jobs, fn($job)  => $job['id'] == $id);
+
+        return view('jobs' , ['job' => $job]);
 });
-Route::get('/contact' , function(){
+Route::get('/contact', function () {
     return view('contact');
 });
